c0f77419d386c682b2adf786cfb46c0f5b408109
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2017-10-24 15:45:15 -0500

    Do not print our newline

--- tests/ansible/run_all_molecule_tests.sh
+++ tests/ansible/run_all_molecule_tests.sh
@@ -5,7 +5,7 @@ max_xargs_processes=5
 
 roles_to_test=$(find ansible/roles/ -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 #roles_to_test=ami-cleanup
-echo "Found the following roles to test:\n${roles_to_test}"
+echo -e "Found the following roles to test:\n${roles_to_test}"
 
 test_runner="$(dirname $0)/run_molecule_tests.sh"
 echo "${roles_to_test}" | xargs -I {} -P${max_xargs_processes} ${test_runner} {}
