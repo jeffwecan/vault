94f6dfadeffb84c82b077253673ffedcefcfd55b
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2017-10-25 22:56:31 -0500

    Subtle tag tweaks

--- tests/ansible/run_molecule_tests.sh
+++ tests/ansible/run_molecule_tests.sh
@@ -6,21 +6,21 @@ role_path="ansible/roles/${role_to_test}"
 
 pushd "${role_path}" >/dev/null
 
-echo "[molecule ${role_to_test}]: Setting up test instance"
+echo "[molecule] [role: ${role_to_test}]: Setting up test instance"
 molecule destroy 2>&1 >/dev/null
 
-echo "[molecule ${role_to_test}]: Running initial provisioning playbook sans bootstrap tags"
-molecule converge -- --skip-tags=bootstrap | awk -v "role=${role_to_test}" '{print "[molecule " role "] [skip-tags=bootstrap]: " $0}'
+echo "[molecule] [role: ${role_to_test}]: Running initial provisioning playbook sans bootstrap tags"
+molecule converge -- --skip-tags=bootstrap | awk -v "role=${role_to_test}" '{print "[molecule] [" role "] [skip-tags=bootstrap]: " $0}'
 
-echo "[molecule ${role_to_test}]: Running initial provisioning playbook with only bootstrap tags"
-molecule converge -- --tags=bootstrap | awk -v "role=${role_to_test}" '{print "[molecule " role "] [tags=bootstrap]: " $0}'
+echo "[molecule] [role: ${role_to_test}]: Running initial provisioning playbook with only bootstrap tags"
+molecule converge -- --tags=bootstrap | awk -v "role=${role_to_test}" '{print "[molecule] [" role "] [tags=bootstrap]: " $0}'
 
 molecule idempotence >/dev/null
 
-echo "[molecule ${role_to_test}]: Verifying / testing test instance"
-molecule verify | awk -v "role=${role_to_test}" '{print "[molecule " role "] [verify]: " $0}'
+echo "[molecule] [role: ${role_to_test}]: Verifying / testing test instance"
+molecule verify | awk -v "role=${role_to_test}" '{print "[molecule] [" role "] [verify]: " $0}'
 
-echo "[molecule ${role_to_test}]: Destroying test instance"
+echo "[molecule] [role: ${role_to_test}]: Destroying test instance"
 molecule destroy 2>&1 >/dev/null
 
 popd >/dev/null
