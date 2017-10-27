a0513eca35563e66fecf0a17f32bdf9cbaafd8ec
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2017-10-26 19:52:31 -0500

    At least only let private ips...

--- terraform/aws/development/main.tf
+++ terraform/aws/development/main.tf
@@ -57,7 +57,7 @@ module "vault_cluster" {
   # recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
 
   allowed_ssh_cidr_blocks            = ["${var.cm_cidr}"]
-  allowed_inbound_cidr_blocks        = ["${var.cm_cidr}"]
+  allowed_inbound_cidr_blocks        = ["172.16.0.0/12"]
   allowed_inbound_security_group_ids = []
   ssh_key_name                       = "${var.ssh_key_name}"
 }
@@ -117,7 +117,7 @@ module "consul_cluster" {
   # deployment, we strongly recommend you limit this to the IP address ranges of known, trusted servers inside your VPC.
 
   allowed_ssh_cidr_blocks            = ["${var.cm_cidr}"]
-  allowed_inbound_cidr_blocks = ["${var.cm_cidr}"]
+  allowed_inbound_cidr_blocks = ["172.16.0.0/12"]  # TODO THIS IS ALL FUCKD
   ssh_key_name                = "${var.ssh_key_name}"
 }
 
