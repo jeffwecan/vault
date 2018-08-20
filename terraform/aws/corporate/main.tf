b6ad81558e325047a7bc0d67fb9acc8ba0335507
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-08-20 11:15:31 -0500

    terraform fmt

--- terraform/aws/corporate/main.tf
+++ terraform/aws/corporate/main.tf
@@ -10,9 +10,10 @@ terraform {
 }
 
 provider "aws" {
-  alias = "corporate"
+  alias   = "corporate"
   version = "~> 1.13.0"
-  region = "${var.aws_corporate_region}"
+  region  = "${var.aws_corporate_region}"
+
   assume_role {
     role_arn     = "${var.aws_corporate_role_arn}"
     session_name = "terraform_corporate_vault_networking"
@@ -20,38 +21,39 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias = "corporate_dns"
+  alias   = "corporate_dns"
   version = "~> 1.13.0"
-  region = "${var.aws_corporate_region}"
+  region  = "${var.aws_corporate_region}"
+
   assume_role {
     role_arn     = "${var.aws_corporate_dns_role_arn}"
     session_name = "terraform_corporate_vault_dns"
   }
 }
 
-
 module "corporate_core_metrics_to_vault" {
   source = "../../modules/aws-vpc-peering-to-vault-vpc"
 
-  peer_owner_id = "${var.peer_owner_id}"
-  vault_client_subnet_id = "${var.corporate_core_metrics_subnet_id}"
-  vault_client_name = "metricsapp"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  peer_owner_id                                     = "${var.peer_owner_id}"
+  vault_client_subnet_id                            = "${var.corporate_core_metrics_subnet_id}"
+  vault_client_name                                 = "metricsapp"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
-    "aws.vault_client" = "aws.corporate"
+    "aws.vault_client"  = "aws.corporate"
     "aws.vault_cluster" = "aws.corporate"
   }
 }
 
 resource "aws_security_group_rule" "allow_vault_server_to_metricsdb_mysql" {
-  provider                  = "aws.corporate"
-  type                      = "ingress"
-  from_port                 = 3306
-  to_port                   = 3306
-  protocol                  = "tcp"
-  source_security_group_id  = "${var.vault_security_group_id}"
+  provider                 = "aws.corporate"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = "${var.vault_security_group_id}"
 
   security_group_id = "${var.metricsdb_security_group_id}"
 }
@@ -59,14 +61,15 @@ resource "aws_security_group_rule" "allow_vault_server_to_metricsdb_mysql" {
 module "cm_to_vault" {
   source = "../../modules/aws-vpc-peering-to-vault-vpc"
 
-  peer_owner_id = "${var.peer_owner_id}"
-  vault_client_subnet_id = "${var.cm_subnet_id}"
-  vault_client_name = "cm"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  peer_owner_id                                     = "${var.peer_owner_id}"
+  vault_client_subnet_id                            = "${var.cm_subnet_id}"
+  vault_client_name                                 = "cm"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
-    "aws.vault_client" = "aws.corporate"
+    "aws.vault_client"  = "aws.corporate"
     "aws.vault_cluster" = "aws.corporate"
   }
 }
@@ -74,14 +77,15 @@ module "cm_to_vault" {
 module "jenkins_to_vault" {
   source = "../../modules/aws-vpc-peering-to-vault-vpc"
 
-  peer_owner_id = "${var.peer_owner_id}"
-  vault_client_subnet_id = "${var.jenkins_subnet_id}"
-  vault_client_name = "jenkins"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  peer_owner_id                                     = "${var.peer_owner_id}"
+  vault_client_subnet_id                            = "${var.jenkins_subnet_id}"
+  vault_client_name                                 = "jenkins"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
-    "aws.vault_client" = "aws.corporate"
+    "aws.vault_client"  = "aws.corporate"
     "aws.vault_cluster" = "aws.corporate"
   }
 }
@@ -89,14 +93,15 @@ module "jenkins_to_vault" {
 module "zabbix_to_vault" {
   source = "../../modules/aws-vpc-peering-to-vault-vpc"
 
-  peer_owner_id = "${var.peer_owner_id}"
-  vault_client_subnet_id = "${var.zabbix_subnet_id}"
-  vault_client_name = "zabbix"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  peer_owner_id                                     = "${var.peer_owner_id}"
+  vault_client_subnet_id                            = "${var.zabbix_subnet_id}"
+  vault_client_name                                 = "zabbix"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
-    "aws.vault_client" = "aws.corporate"
+    "aws.vault_client"  = "aws.corporate"
     "aws.vault_cluster" = "aws.corporate"
   }
 }
@@ -104,19 +109,21 @@ module "zabbix_to_vault" {
 module "vault_elbv2_dns_record" {
   source = "git@github.com:wpengine/infraform.git//modules/dns-for-aws-elbv2?ref=v1.42"
 
-  name = "${var.vault_dns_record_name}"
+  name              = "${var.vault_dns_record_name}"
   load_balancer_arn = "${var.vault_load_balancer_arn}"
+
   providers = {
-    "aws.dns" = "aws.corporate_dns"
+    "aws.dns"           = "aws.corporate_dns"
     "aws.load_balancer" = "aws.corporate"
   }
 }
 
 resource "aws_vpc_peering_connection" "heroku_wpengine_corporate_space_to_vault" {
-  provider = "aws.corporate"
+  provider      = "aws.corporate"
   peer_owner_id = "${var.heroku_wpengine_corporate_space_peer_owner}"
   peer_vpc_id   = "${var.heroku_wpengine_corporate_space_vpc_id}"
   vpc_id        = "${var.vault_vpc_id}"
+
   tags {
     "CreatingResource" = "github.com/wpengine/vault"
   }
@@ -127,6 +134,7 @@ resource "aws_vpc_peering_connection" "heroku_wpengine_corporate_space_to_vault"
  */
 resource "aws_route" "vault_to_heroku_wpengine_corporate_space_route" {
   provider = "aws.corporate"
+
   # Create one route table entry for each heroku dyno CIDR
   count                     = "${length(var.heroku_wpengine_corporate_space_dyno_cidrs)}"
   route_table_id            = "${var.vault_route_table_id}"
@@ -135,12 +143,12 @@ resource "aws_route" "vault_to_heroku_wpengine_corporate_space_route" {
 }
 
 resource "aws_security_group_rule" "allow_heroku_wpengine_corporate_space_to_vault_loadbalancer" {
-  provider = "aws.corporate"
-  type            = "ingress"
-  from_port       = 443
-  to_port         = 443
-  protocol        = "tcp"
-  cidr_blocks     = ["${var.heroku_wpengine_corporate_space_dyno_cidrs}"]
+  provider    = "aws.corporate"
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["${var.heroku_wpengine_corporate_space_dyno_cidrs}"]
 
   security_group_id = "${var.vault_load_balancer_security_group_id}"
 }
--- terraform/aws/development/main.tf
+++ terraform/aws/development/main.tf
@@ -10,9 +10,10 @@ terraform {
 }
 
 provider "aws" {
-  alias = "development"
+  alias   = "development"
   version = "~> 1.13.0"
-  region = "${var.aws_development_region}"
+  region  = "${var.aws_development_region}"
+
   assume_role {
     role_arn     = "${var.aws_development_role_arn}"
     session_name = "terraform_development_vault_networking"
@@ -20,9 +21,10 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias = "corporate_dns"
+  alias   = "corporate_dns"
   version = "~> 1.13.0"
-  region = "${var.aws_corporate_region}"
+  region  = "${var.aws_corporate_region}"
+
   assume_role {
     role_arn     = "${var.aws_corporate_dns_role_arn}"
     session_name = "terraform_development_vault_dns"
@@ -30,34 +32,35 @@ provider "aws" {
 }
 
 provider "google" {
-  alias = "development"
+  alias   = "development"
   version = "~> 1.1"
   project = "${var.gcp_project}"
-  region = "${var.gcp_region}"
+  region  = "${var.gcp_region}"
 }
 
 module "corporate_core_metrics_to_vault" {
   source = "../../modules/aws-vpc-peering-to-vault-vpc"
 
-  vault_client_name = "metricsapp"
-  vault_client_subnet_id = "${var.corporate_core_metrics_subnet_id}"
-  peer_owner_id = "${var.peer_owner_id}"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  vault_client_name                                 = "metricsapp"
+  vault_client_subnet_id                            = "${var.corporate_core_metrics_subnet_id}"
+  peer_owner_id                                     = "${var.peer_owner_id}"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
-    "aws.vault_client" = "aws.development"
+    "aws.vault_client"  = "aws.development"
     "aws.vault_cluster" = "aws.development"
   }
 }
 
 resource "aws_security_group_rule" "allow_vault_server_to_metricsdb_mysql" {
-  provider                  = "aws.development"
-  type                      = "ingress"
-  from_port                 = 3306
-  to_port                   = 3306
-  protocol                  = "tcp"
-  source_security_group_id  = "${var.vault_security_group_id}"
+  provider                 = "aws.development"
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = "${var.vault_security_group_id}"
 
   security_group_id = "${var.metricsdb_security_group_id}"
 }
@@ -65,30 +68,32 @@ resource "aws_security_group_rule" "allow_vault_server_to_metricsdb_mysql" {
 module "dev_cm_to_vault" {
   source = "../../modules/gcp-vpn-to-vault-vpc"
 
-  vault_client_name = "dev-cm"
-  vault_client_gcp_region = "${var.gcp_region}"
-  vault_client_gcp_network_name = "${var.gcp_network_name}"
-  vault_client_subnet_cidr = "${var.dev_cm_subnet_cidr}"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  vault_client_name                                 = "dev-cm"
+  vault_client_gcp_region                           = "${var.gcp_region}"
+  vault_client_gcp_network_name                     = "${var.gcp_network_name}"
+  vault_client_subnet_cidr                          = "${var.dev_cm_subnet_cidr}"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
     "google.vault_client" = "google.development"
-    "aws.vault_cluster" = "aws.development"
+    "aws.vault_cluster"   = "aws.development"
   }
 }
 
 module "zabbix_to_vault" {
   source = "../../modules/aws-vpc-peering-to-vault-vpc"
 
-  peer_owner_id = "${var.peer_owner_id}"
-  vault_client_subnet_id = "${var.zabbix_subnet_id}"
-  vault_client_name = "zabbix"
-  vault_vpc_id = "${var.vault_vpc_id}"
+  peer_owner_id                                     = "${var.peer_owner_id}"
+  vault_client_subnet_id                            = "${var.zabbix_subnet_id}"
+  vault_client_name                                 = "zabbix"
+  vault_vpc_id                                      = "${var.vault_vpc_id}"
   vault_application_load_balancer_security_group_id = "${var.vault_load_balancer_security_group_id}"
-  vault_route_table_id = "${var.vault_route_table_id}"
+  vault_route_table_id                              = "${var.vault_route_table_id}"
+
   providers = {
-    "aws.vault_client" = "aws.development"
+    "aws.vault_client"  = "aws.development"
     "aws.vault_cluster" = "aws.development"
   }
 }
@@ -96,10 +101,11 @@ module "zabbix_to_vault" {
 module "vault_elbv2_dns_record" {
   source = "git@github.com:wpengine/infraform.git//modules/dns-for-aws-elbv2?ref=v1.42"
 
-  name = "${var.vault_dns_record_name}"
+  name              = "${var.vault_dns_record_name}"
   load_balancer_arn = "${var.vault_load_balancer_arn}"
+
   providers = {
-    "aws.dns" = "aws.corporate_dns"
+    "aws.dns"           = "aws.corporate_dns"
     "aws.load_balancer" = "aws.development"
   }
 }
